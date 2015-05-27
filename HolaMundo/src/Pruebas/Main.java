c57967d6829f5b2aa45c88d1c9c1d58110ddb301
Xsmxxl Uchiha <ismael-uchiha1@hotmail.com>
2015-05-26 21:26:27 -0500

    Repositoriao 26/5/15 9:26pm

---
+++ HolaMundo/src/Pruebas/Main.java
@@ -0,0 +1,14 @@
+package Pruebas;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+	}
+
+}
---
+++ HolaMundo/src/Pruebas/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Ismael
+ *
+ */
+package Pruebas;
